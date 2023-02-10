d5f45f1e46966d3a1a3e297ab7606a7471c51b7b
Angel <angel.36b@gmail.com>
2023-02-09 23:10:07 -0500

    se agrega boton de reset, se establece el spread operator para el estado board y se define la funcion de resetGame

--- triqui/src/App.jsx
+++ triqui/src/App.jsx
@@ -1,24 +1,24 @@
 import './App.css'
 import { useState } from 'react'
-// import './index.css'
+import './index.css'
 
 
-const TURNS ={
+const TURNS = {
   X: 'x',
-  O:'o'
+  O: 'o'
 }
 
 
 
-const Square =({ children, isSelected, updateBoard, index })=>{
+const Square = ({ children, isSelected, updateBoard, index }) => {
 
   const className = `square ${isSelected ? 'is-selected' : ''}`
   //llamar la version 
 
-  const handleClick = () =>{
+  const handleClick = () => {
     updateBoard(index)
   }
-  
+
   return (
     <div onClick={handleClick} className={className}>
       {children}
@@ -26,20 +26,20 @@ const Square =({ children, isSelected, updateBoard, index })=>{
   )
 }
 
-const WINNE_COMBOS = [
-  [0, 1, 2]
-  [3, 4, 5]
-  [6, 7, 8]
-  [0, 3, 6]
-  [1, 4, 7]
-  [2, 5, 8]
-  [0, 4, 8]
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
   [2, 4, 6]
 ]
 
-function App(){
+function App() {
 
-  const [board, setBoard] = useState( Array(9).fill(null))
+  const [board, setBoard] = useState(Array(9).fill(null))
 
   const [turn, setTurn] = useState(TURNS.X)
 
@@ -48,71 +48,103 @@ function App(){
   const checkWinner = (boardToCheck) => {
 
     //Revisamos y validamos las combinaciones ganadores
-    for(const combo of WINNER_COMBOS){
+    for (const combo of WINNER_COMBOS) {
       const [a, b, c] = combo
-      if(
+      if (
         boardToCheck[a] &&
-        boardToCheck[a] ==boardToCheck[b] &&
-        boardToCheck[a] ==boardToCheck[c]
-      ){
+        boardToCheck[a] == boardToCheck[b] &&
+        boardToCheck[a] == boardToCheck[c]
+      ) {
         return boardToCheck[a]
       }
     }
 
   }
-  
-  
-  const updateBoard = (index) =>{
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+
+
+  const updateBoard = (index) => {
     //Bloqueamos la actualizacion de valor del index, cuando ya cuenta con un valos
-    if(board[index]) return
+    if (board[index]||winner) return
 
-      const newBoard = [...board] //se pasa el array a un nuevo array (spread y rest operator) los estados se tratan siempre inmutable
-      newBoard[index] = turn
-      setBoard(newBoard)
+    const newBoard = [...board] //se pasa el array a un nuevo array (spread y rest operator) los estados se tratan siempre inmutable
+    newBoard[index] = turn
+    setBoard(newBoard)
 
-      const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X
-      setTurn(newTurn)
+    const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
 
-      //revisamos si hay ganador
-      const newWinner = checkWinner(newBoard)
-      if (newWinner){
-        alert(`El ganador es ${newWinner}` )
-        setWinner(newWinner)
-      }
+    //revisamos si hay ganador
+    const newWinner = checkWinner(newBoard)
+
+    if (newWinner) {
+
+      setWinner(newWinner)//actualiza el estado
+      
+    }
+    //check if game is over
+
 
   }
 
-    return (
-    
+  return (
+
     <main className='board'>
       <h1> Triqui </h1>
+      <button> Reset del juego </button>
       <section className='game'>
         {
-          board.map((_,index) => {
-            return(
+          board.map((_, index) => {
+            return (
 
-             <Square 
-              key={index}
-              index={index} 
-              updateBoard={updateBoard}
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
               >
                 {board[index]}
-             </Square>
+              </Square>
             )
           })
         }
       </section>
 
       <section className='turn'>
-          <Square isSelected={turn == TURNS.X}> 
+        <Square isSelected={turn == TURNS.X}>
           {TURNS.X}
-          </Square>
-          <Square isSelected={turn == TURNS.O}>
-            {TURNS.O}
-          </Square>
+        </Square>
+        <Square isSelected={turn == TURNS.O}>
+          {TURNS.O}
+        </Square>
       </section>
+
+      {
+        winner ===! null && (
+          <section className="winner">
+            <div className="text">
+              <h2>
+                {winner == false ? 'Empate' : 'Gano:'}
+              </h2>
+              <header className='Win'>
+                {winner && <Square> {winner} </Square>}
+              </header>
+              <footer>
+
+                <button onClick={restGame}> Empezar de nuevo </button>
+
+              </footer>
+            </div>
+
+          </section>
+        )
+      }
     </main>
-    )
+  )
 }
 
 export default App
